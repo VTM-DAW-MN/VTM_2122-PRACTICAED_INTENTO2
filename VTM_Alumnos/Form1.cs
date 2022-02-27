014c1197a22d1044e0b9a52b840334736f683fbc
SentoTM <vicent.tormo@gmail.com>
2022-02-27 20:31:05 +0100

    Distingue 4 notas

--- VTM_Alumnos/Form1.cs
+++ VTM_Alumnos/Form1.cs
@@ -24,13 +24,25 @@ namespace VTM_Alumnos
         private void button1_Click(object sender, EventArgs e)
         {
             Alumno miAlumno = new Alumno();
-            String miAlumnoStr;
+             String miAlumnoStr, miAlumnoNotaTexto;
+             miAlumno.Nombre = aluNombre.Text;
+             miAlumno.Nota = Convert.ToInt32(aluNota.Text);
+            if (miAlumno.Nota<5) {
+            miAlumnoNotaTexto = "Suspenso";
+            }
+            else if (miAlumno.Nota<7) {
+            miAlumnoNotaTexto = "Aprobado";
+            }
+            else if (miAlumno.Nota<9) {
+            miAlumnoNotaTexto = "Notable";
+            }
+            else
+            miAlumnoNotaTexto = "Sobresaliente";
+             miAlumnoStr = aluNombre.Text + " " + aluNota.Text + " " +
+            miAlumnoNotaTexto +"\n";
+             listaAlumnos.AppendText(miAlumnoStr);
+             misAlumnos.Agregar(miAlumno);
 
-            miAlumno.Nombre = aluNombre.Text;
-            miAlumno.Nota = Convert.ToInt32(aluNota.Text);
-            miAlumnoStr = aluNombre.Text + " " + aluNota.Text + (miAlumno.Aprobado ? " Aprobado" : " Suspenso") + "\n";
-            listaAlumnos.AppendText(miAlumnoStr);
-            misAlumnos.Agregar(miAlumno);
         }
     }
 
